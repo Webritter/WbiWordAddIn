20e7ac6958ec899e6404aacf619920e57cf3d192
webritter@gmx.at <webritter@gmx.at>
2017-02-14 11:30:02 +0100

    es6-promise in app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -13,6 +13,9 @@ import { default as DocumentContainer } from './containers/document-container/in
 import { store } from './store/index';
 const history = syncHistoryWithStore(hashHistory, store) as any;
 
+import { Promise } from 'es6-promise';
+
+
 
 function App() {
   return (
